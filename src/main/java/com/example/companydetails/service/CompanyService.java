62402fce51a949a818aa4c4cc7e5bbaecf4ea3b5
Manojgowdab <Manoj.gowda@inflowsol.com>
2025-10-09 11:52:54 +0530

    Update CompanyService.java

--- src/main/java/com/example/companydetails/service/CompanyService.java
+++ src/main/java/com/example/companydetails/service/CompanyService.java
@@ -16,5 +16,9 @@ public class CompanyService {
     {
         return companyRepository.findAll();
     }
+    public String getCompanyOwnerNameByCompanyName(String CompanyName)
+    {
+        return companyRepository.getOwnerName();
+    }
 
 }
